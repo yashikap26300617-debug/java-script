f9447841a28a68aa0685c83f9c4753303e4f8c82
yashikap26300617 <yashikap.26300617@kalvium.community>
2026-09-10 06:59:30 +0530

    initial code

---
+++ WEEK1/DAY1.js
@@ -0,0 +1,16 @@
+// console.log("Hello, World!");
+
+// process.stdin.on("data",function(data){
+//     data=data.toString().split("\n");
+//     let n=data[0];
+//     let p=data[2];
+
+// })
+
+
+// process.stdin.on("data",function(data){
+//     data=data.toString().split("\n");
+//     let n=data[0];
+//     let arr=data[1].split(" ").map(Number);
+
+// })
---
+++ WEEK1/day2.js
@@ -0,0 +1,29 @@
+// let arr=["a","b","c","a","b","d",];
+// let obj={}
+// for(let i=0;i<arr.length;i++){
+//   if(obj[arr[i]]==undefined){
+//     obj[arr[i]]=1;
+//   }
+//   else{
+//       obj[arr[i]]+=1;
+//   }
+// }
+// console.log(obj)
+
+
+// let num=[1,2,3,4,5];
+// let ans=num.map((item)=>item+1);
+// let ans2=num.filter((item)=>item%2==0)
+// console.log(ans2)
+
+
+let que=[1,2,3,4,5,6];
+// yes \no
+let isPresent=false;
+for(let i=0;i<que.length;i++){
+    if(que[i]==4){
+        isPresent=true;
+    }
+   
+}
+console.log(isPresent?"yes":"no")
